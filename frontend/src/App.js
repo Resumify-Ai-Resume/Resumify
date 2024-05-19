ab1ecaaa8c46bfb09b26e81eef43b0b2d2f8bc42
Yashank Bhola <yashankbhola@hotmail.com>
2024-05-18 21:01:06 -0400

    Revert "Gay"
    This reverts commit 139a86899450448a17ba4511e99aa9be6eace292.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,13 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Layout/Navbar';
-import Dashboard from './components/dashboard/Dashboard';
-import GeneralPage from './components/form/GeneralPage/GeneralPage';
-import EducationPage from './components/form/EducationPage/EducationPage';
-import SkillPage from './components/form/SkillPage/SkillPage';
-import WorkExpPage from './components/form/WorkExpPage/WorkExpPage';
-import ProjectPage from './components/form/ProjectPage/ProjectPage';
-import CourseWorkPage from './components/form/CourseWorkPage/CourseWorkPage';
-import ResumePage from './components/form/Resume/ResumePage';
+import React, {Component} from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Layout/Navbar.js'
 
 class App extends Component {
-  render() {
+  render (){
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/form/general' element={<GeneralPage />} />
-            <Route path='/form/education' element={<EducationPage />} />
-            <Route path='/form/skill' element={<SkillPage />} />
-            <Route path='/form/workexp' element={<WorkExpPage />} />
-            <Route path='/form/project' element={<ProjectPage />} />
-            <Route path='/form/coursework' element={<CourseWorkPage />} />
-            <Route path='/form/resume' element={<ResumePage />} />
-          </Routes>
         </div>
       </BrowserRouter>
     );
